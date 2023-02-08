b077c2156d5dbaf0de188e8605a90af509bacf55
Jyoti-Mish79 <jyotimishra2996@gmail.com>
2023-02-08 09:53:41 +0530

    solo

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// Your JS code here.
+<script>
+  const images = document.querySelectorAll(".slide-in");
+
+  function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function() {
+      let context = this, args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function checkSlide() {
+    images.forEach(image => {
+      const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+      const imageBottom = image.offsetTop + image.height;
+      const isHalfShown = slideInAt > image.offsetTop;
+      const isNotScrolledPast = window.scrollY < imageBottom;
+      if (isHalfShown && isNotScrolledPast) {
+        image.classList.add("active");
+      } else {
+        image.classList.remove("active");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", debounce(checkSlide));
+</script>
+
